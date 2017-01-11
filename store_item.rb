537907cfa71732705f75bfdd80473a6b6dab2643
Dieudonne Mukunzi <dmukunzi0@gmail.com>
2017-01-11 14:28:26 +0200

    Initial commit

---
+++ store_item.rb
@@ -0,0 +1,10 @@
+item1 = {
+	iname: "Bacon", weight: 100, price: 500, category: "food" 
+}
+
+item2 = {
+	iname: "Tuna", weight: 10, price: 1500, category: "fish" 
+}
+item3 = {
+	"iname" => "iPhone 7 Plus", "Weight" => 0.9, "category" => "electronics"
+}
